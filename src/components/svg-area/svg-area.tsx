cf0a4c2e218dce1f21be5360e173d8e1bb69caca
“tlvu93” <101603072+tlvu93@users.noreply.github.com>
2022-07-05 09:18:46 +0200

    Fix SVG area with id selector

--- src/components/svg-area/svg-area.tsx
+++ src/components/svg-area/svg-area.tsx
@@ -1,13 +1,13 @@
-import * as d3 from "d3";
-import { useEffect } from "react";
-import { useTaskContext } from "src/context/task-context";
-import { v4 as uuid } from "uuid";
+import * as d3 from 'd3';
+import { useEffect } from 'react';
+import { useTaskContext } from 'src/context/task-context';
+import { v4 as uuid } from 'uuid';
 
-import { ModalType, useModal } from "@context/modal-context";
-import { useWorkflowContext } from "@context/workflow-context";
+import { ModalType, useModal } from '@context/modal-context';
+import { useWorkflowContext } from '@context/workflow-context';
 
-import { convertCoordinatesDOMtoSVG } from "../../utils/convertCoordinatesDOMtoSVG";
-import SVGDrawer from "./svg-drawer";
+import { convertCoordinatesDOMtoSVG } from '../../utils/convertCoordinatesDOMtoSVG';
+import SVGDrawer from './svg-drawer';
 
 const SVGArea = () => {
   const { draggedTask } = useTaskContext();
@@ -36,28 +36,28 @@ const SVGArea = () => {
 
   const onDragOver = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
-    d3.select("svg").classed("drag-over", true);
+    d3.select('#svg-area').classed('drag-over', true);
   };
 
   const onDragLeave = () => {
-    d3.select("svg").classed("drag-over", false);
+    d3.select('#svg-area').classed('drag-over', false);
   };
 
   const onDrop = (e: React.DragEvent<HTMLDivElement>) => {
     e.stopPropagation();
 
-    d3.select("svg").classed("drag-over", false);
+    d3.select('#svg-area').classed('drag-over', false);
 
     // Check if the dropped Element is a dropableElement
-    const identifier = e.dataTransfer.getData("text");
-    if (identifier !== "dropableElement") {
+    const identifier = e.dataTransfer.getData('text');
+    if (identifier !== 'dropableElement') {
       return;
     }
 
     // Get the correct coordinates for this node
     const dragData = draggedTask as DraggedData;
     const { x, y } = convertCoordinatesDOMtoSVG(
-      d3.select("svg"),
+      d3.select('#svg-area'),
       e.clientX - dragData.offset[0],
       e.clientY - dragData.offset[1]
     );
@@ -77,12 +77,12 @@ const SVGArea = () => {
 
   return (
     <div
-      className="m-1 h-[calc(100vh_-_18.6rem)]"
+      className='m-1 h-[calc(100vh_-_18.6rem)]'
       onDrop={(e) => onDrop(e)}
       onDragLeave={() => onDragLeave()}
       onDragOver={(e) => onDragOver(e)}
     >
-      <svg className="h-full w-full"></svg>
+      <svg id='svg-area' className='h-full w-full'></svg>
     </div>
   );
 };
--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,18 +1,19 @@
-import React, { ReactNode } from "react";
-import Head from "next/head";
-import Header from "@components/header/header";
+import Head from 'next/head';
+import React, { ReactNode } from 'react';
+
+import Header from '@components/header/header';
 
 type Props = {
   children?: ReactNode;
   title?: string;
 };
 
-const Layout = ({ children, title = "This is the default title" }: Props) => (
-  <div className="flex min-h-screen flex-col bg-gray-100">
+const Layout = ({ children, title = 'This is the default title' }: Props) => (
+  <div className='flex min-h-screen flex-col bg-gray-100'>
     <Head>
       <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta charSet='utf-8' />
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
     </Head>
 
     <Header />
